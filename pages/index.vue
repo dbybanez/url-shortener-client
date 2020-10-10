23523524f7518a3dacd944150e15b97518c8cd08
dbybanez <dbybanez@gmail.com>
2020-10-10 23:04:20 +0800

    structuring home layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <button class="btn btn-primary">Hello</button>
+  <div class="site-content vh-100">
+    <section class="content-section h-100">
+      <div class="container-fluid h-100">
+        <div class="row align-items-center justify-content-center h-100">
+          <div class="col-12 col-md-5 col-lg-4 col-xl-3 text-center">
+            <h1 class="text-rbl mb-5">
+              tinyy.link
+            </h1>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
