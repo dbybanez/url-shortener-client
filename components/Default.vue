197255e6154be97fa1dcb876e4a3e6da30c0e535
dbybanez <dbybanez@gmail.com>
2020-10-12 22:50:21 +0800

    added form validation using yup

--- components/Default.vue
+++ components/Default.vue
@@ -1,20 +1,55 @@
 <template>
   <div class="form">
     <div class="form-group">
-      <input type="text" class="form-control" placeholder="URL">
+      <input
+        type="text"
+        class="form-control"
+        :class="(!urlError) ? '' : 'is-invalid'"
+        placeholder="URL"
+        v-model="url">
+      <div v-if="urlError" class="invalid-feedback">
+        {{ urlError }}
+      </div>
     </div>
     <div class="form-group">
       <input type="text" class="form-control" placeholder="Slug">
     </div>
-    <p class="mb-1">Preview</p>
-    <h4 class="mb-5">https://tinyy.link/slug</h4>
-    <button class="btn btn-primary btn-block">Generate a tinyy.link</button>
+    <p class="mb-1 text-center">Preview:</p>
+    <h4 class="mb-5 text-center"><code>https://tinyy.link/slug</code></h4>
+    <button
+      class="btn btn-primary btn-block"
+      @click="validateURL">Generate a tinyy.link</button>
   </div>
 </template>
 
 <script>
+import * as Yup from 'yup'
+
 export default {
-  name: 'Default'
+  name: 'Default',
+  data () {
+    return {
+      url: '',
+      urlError: null,
+      slugError: null
+    }
+  },
+  methods: {
+    async validateURL () {
+      const url = this.url
+      const schema = Yup.object().shape({
+        url: Yup.string().trim().url('Oops, please provide a valid URL.').required('Are we forgetting something? Please provide a URL')
+      })
+      try {
+        await schema.validate({
+          url
+        })
+        this.urlError = null
+      } catch (error) {
+        this.urlError = error.message
+      }
+    }
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <section class="content-section h-100">
       <div class="container-fluid h-100">
         <div class="row align-items-center justify-content-center h-100">
-          <div class="col-12 col-md-5 col-lg-4 col-xl-3 text-center">
-            <h1 class="text-rbl mb-5">
+          <div class="col-12 col-md-5 col-lg-4 col-xl-3">
+            <h1 class="text-rbl mb-5 text-center">
               Logo
             </h1>
             <Default />
