e2098a3aadffcfddea4236a60506b82128d5c864
dbybanez <dbybanez@gmail.com>
2020-11-09 21:29:36 +0800

    added edit page

---
+++ components/Edit.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="form">
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        placeholder=""
+        aria-label=""
+        aria-describedby="generated-link"
+        :value="getID"
+        readonly>
+    </div>
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Code">
+    </div>
+    <button
+      class="btn btn-primary btn-block">Edit URL</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Default',
+  data () {
+    return {
+      url: '',
+      slug: ''
+    }
+  },
+  methods: {
+    ...mapActions({
+      validate: 'validate',
+      resetURL: 'resetURL',
+      resetSlug: 'resetSlug'
+    })
+  },
+  computed: {
+    ...mapGetters({
+      urlHasError: 'getUrlHasError',
+      urlErrorMessage: 'getUrlErrorMessage',
+      slugHasError: 'getSlugHasError',
+      slugErrorMessage: 'getSlugErrorMessage'
+    }),
+    getID () {
+      return 'https://tinyy.link/' + this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ pages/_id/edit/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="site-content vh-100">
+    <section class="content-section h-100">
+      <div class="container-fluid h-100">
+        <div class="row align-items-center justify-content-center h-100">
+          <div class="col-12 col-md-5 col-lg-4 col-xl-3">
+            <h1 class="text-rbl mb-5 text-center">
+              Logo
+            </h1>
+            <Edit />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Edit from '../../../components/Edit'
+
+export default {
+  components: {
+    Edit
+  },
+  computed: {
+    ...mapGetters({
+      getGeneratedStatus: 'getGeneratedStatus'
+    })
+  }
+}
+</script>
+
+<style></style>
