eb79485b3e60150d97482e14853117a653757a7b
dbybanez <dbybanez@gmail.com>
2020-11-10 22:32:34 +0800

    updated 404 messaing, added comments for redirect page

--- components/404.vue
+++ components/404.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    404
+    <h1>🙁</h1>
+    <h1 class="mb-4">404</h1>
+    <p class="mb-0">We tried our best, but we couldn't find your url.</p>
+    <p class="mb-5">Would you like to generate one instead?</p>
+    <a href="/" class="btn btn-primary">Generate</a>
   </div>
 </template>
 
--- pages/_id/index.vue
+++ pages/_id/index.vue
@@ -4,11 +4,11 @@
       <div class="container-fluid h-100">
         <div class="row align-items-center justify-content-center h-100">
           <div class="col-12 col-md-5 col-lg-4 col-xl-3 text-center">
-            <div class="spinner-border mb-3" role="status">
+            <div v-if="found" class="spinner-border mb-3" role="status">
               <span class="sr-only">Loading...</span>
             </div>
-            <h5>Supercharging your URL...</h5>
-            <PageError />
+            <h5 v-if="found">Supercharging your URL...</h5>
+            <PageError v-if="!found"/>
           </div>
         </div>
       </div>
@@ -22,14 +22,25 @@ export default {
   components: {
     PageError
   },
+  data () {
+    return {
+      found: true
+    }
+  },
   async mounted () {
     try {
+      // get slug
       const slug = this.$route.params.id
+      // look for slug in database
       const url = await 'https://google.com'
+      // if it exists, redirect
       if (slug === 'QWERTY') {
-        window.location = url
+        setTimeout(() => {
+          window.location = url
+        }, 3000)
       } else {
-        console.log('Not found: 404')
+        // else, show 404
+        this.found = false
       }
     } catch (error) {
       console.log(error)
